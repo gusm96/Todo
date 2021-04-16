ab61c6de983a4c83dcaed0db7feded8a3d761a48
Seongmo Gu <gusm96@naver.com>
2021-04-16 15:42:33 +0900

    Saving Users Name JS

--- clock.js
+++ clock.js
@@ -1,19 +1,23 @@
+// Clock Form
 const clock = document.querySelector(".clock");
 const days = document.querySelector(".days");
 
+// Clock Function
 function getNowDate() {
   const now = new Date();
   const hours = now.getHours();
   const minutes = now.getMinutes();
   const year = now.getFullYear();
-  const month = ("0" + (1 + now.getMonth())).slice(-2);
-  const date = ("0" + now.getDate()).slice(-2);
+  const month = now.getMonth() + 1;
+  const date = now.getDate();
   const WEEKDAY = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
   const day = WEEKDAY[now.getDay()];
   clock.innerText = `${hours < 10 ? `0${hours}` : hours}:${
     minutes < 10 ? `0${minutes}` : minutes
   }`;
-  days.innerText = `${year}-${month}-${date}-${day}`;
+  days.innerText = `${year}-${month < 10 ? `0${month}` : month}-${
+    date < 10 ? `0${date}` : date
+  }-${day}`;
 }
 
 function init() {
---
+++ usersName.js
@@ -0,0 +1,43 @@
+// Users Name Form
+const nameForm = document.querySelector(".name_form"),
+  nameInput = nameForm.querySelector(".name_write");
+const usersName = document.querySelector(".users_name");
+const SHOWING_CN = "showing";
+const USERS_LS = "Current-Users";
+
+// Saving Users name function
+
+function saveUsersName(text) {
+  localStorage.setItem(USERS_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currnetValue = nameInput.value;
+  greeting(currnetValue);
+  saveUsersName(currnetValue);
+}
+
+function askForName() {
+  nameForm.classList.add(SHOWING_CN);
+  nameForm.addEventListener("submit", handleSubmit);
+}
+
+function greeting(text) {
+  nameForm.classList.remove(SHOWING_CN);
+  usersName.classList.add(SHOWING_CN);
+  usersName.innerText = `Hello ${text}`;
+}
+function loadUsersName() {
+  const cuurentUser = localStorage.getItem(USERS_LS);
+  if (cuurentUser === null) {
+    askForName();
+  } else {
+    greeting(cuurentUser);
+  }
+}
+
+function init() {
+  loadUsersName();
+}
+init();
